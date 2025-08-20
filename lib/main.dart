ed0d72c050d283d37ef2e80c8cd263d84721a148
BENNET DYANI <b.dyani@plum.systems.com>
2025-08-21 01:31:43 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+//import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
+//import 'package:authentication_app/auth_screen.dart';
+import 'package:shuttle_tracker/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Authentication App',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(), // HomeScreen as the home widget
+    );
+  }
+}
+
+
+
+
+
